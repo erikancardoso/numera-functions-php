92aab9f30124f9d4321e5cb3791ecd8d799ee7cc
Erika Maria N. Cardoso <69882736+erikancardoso@users.noreply.github.com>
2024-09-13 13:16:06 -0400

    Update endereco.php

--- CalculadoraCabala/endereco.php
+++ CalculadoraCabala/endereco.php
@@ -1,1 +1,13 @@
+$tabelaVibracaoResidencial [
+1 => 'Individualismo',
+2 => 'Sensibilidade e Harmonia',
+3 => 'Expressividade e Festa',
+4 => 'Estabilidade e Trabalho',
+5 => 'Movimentação e Mobilidade',
+6 => 'Familia e Amor',
+7 => 'Concentração e Introspeção', 
+8 => 'Materialismo e Justiça', 
+9 => 'Humanitarismo e Generosidade'
+]
+
 
