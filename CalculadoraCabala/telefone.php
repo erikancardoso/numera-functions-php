d111e97b4f749cff3b66be7d152d3df331bef62b
Erika Maria N. Cardoso <69882736+erikancardoso@users.noreply.github.com>
2025-05-10 00:54:17 -0400

    Update telefone.php

--- CalculadoraCabala/telefone.php
+++ CalculadoraCabala/telefone.php
@@ -1,1 +1,16 @@
+<?php
+
+function calcularVibracaoTelefone($telefone) {
+    $numeros = preg_replace('/\D/', '', $telefone);
+    $soma = array_sum(str_split($numeros));
+
+    while (!in_array($soma, [11, 22]) && $soma > 9) {
+        $soma = array_sum(str_split($soma));
+    }
+
+    return $soma;
+}
+
+$telefone = "49999659555";
+echo "Vibração do telefone $telefone: " . calcularVibracaoTelefone($telefone);
 
